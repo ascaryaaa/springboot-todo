a9a3ca4b037612e8fb3831763930ce7dd7def79a
ascaryaaa <allaamascarya@gmail.com>
2024-02-20 14:59:33 +0700

    initial commit

---
+++ src/main/java/com/app/todo/enums/TodoStatus.java
@@ -0,0 +1,4 @@
+package com.app.todo.enums;
+
+public class TodoStatus {
+}
---
+++ src/main/java/com/app/todo/model/Todo.java
@@ -0,0 +1,38 @@
+package com.app.todo.model;
+
+import com.app.todo.enums.TodoStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    Long id;
+    @Column
+    String description;
+    @Column
+    TodoStatus todoStatus;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    Timestamp dateCreated;
+
+    @UpdateTimestamp
+    Timestamp lastModified;
+}
